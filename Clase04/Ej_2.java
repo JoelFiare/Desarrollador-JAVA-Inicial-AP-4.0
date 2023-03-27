b19c144585dcb1355f9e3eee518a10d91ca6aca8
JoelFiare <JoelFiare@gmail.com>
2023-03-27 15:03:11 -0300

    Ejercicio 2 y archivo txt

---
+++ Clase04/Ej_2.java
@@ -0,0 +1,37 @@
+// Autor: Joel Fiaré
+// Imprime en pantalla:
+// La suma de los números en el archivo es: 15
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Ej_2 {
+    public static void main(String[] args) {
+        String filePath = "I:\\Mi unidad\\Repositorios JoelFiare Git\\Desarrollador-JAVA-Inicial-AP-4.0\\Clase04\\archivo.txt";
+
+        try {
+            // Abrir el archivo y crear un lector de texto
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+            int sum = 0;
+
+            // Leer cada línea del archivo
+            while ((line = bufferedReader.readLine()) != null) {
+                // Convertir la línea a un número entero y sumarlo
+                int number = Integer.parseInt(line);
+                sum += number;
+            }
+
+            // Cerrar el lector de texto y mostrar la suma de los números
+            bufferedReader.close();
+            System.out.println("La suma de los números en el archivo es: " + sum);
+
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error al leer el archivo: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("El archivo contiene un número inválido: " + e.getMessage());
+        }
+    }
+}
