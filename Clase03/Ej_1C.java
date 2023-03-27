78b10ef553cd262c47fa135390a360458f39359f
JoelFiare <JoelFiare@gmail.com>
2023-03-27 10:31:12 -0300

    Ejercicio 1C Completado

---
+++ Clase03/Ej_1C.java
@@ -0,0 +1,20 @@
+// Autor: Joel Fiaré
+// Imprime en pantalla:
+// La suma de los números mayores a 5 es 32
+public class Ej_1C {
+    public static void main(String[] args) {
+        int[] numeros = {4, 7, 2, 9, 1, 16};
+        int x = 5;
+        int suma = 0;
+
+        // Sumar todos los números mayores a x
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] > x) {
+                suma += numeros[i];
+            }
+        }
+
+        // Imprimir la suma
+        System.out.println("La suma de los números mayores a " + x + " es " + suma);
+    }
+}
