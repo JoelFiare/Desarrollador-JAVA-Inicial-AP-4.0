ce62320bdec54f30a03946f64049d26d684d637e
JoelFiare <JoelFiare@gmail.com>
2023-04-19 11:57:25 -0300

    actualizando el cuestionario de repaso

--- CodigoDeLaClaseOnline/Test.java
+++ CodigoDeLaClaseOnline/Test.java
@@ -1,2 +1,28 @@
-package CodigoDeLaClaseOnline;public class Test {
+package CodigoDeLaClaseOnline;
+
+import java.io.OptionalDataException;
+
+public class Test {
+    public static void main(String[] args) {
+        String unaPalabra = "unaPalabra";
+        String unaPalabra1 = "unaPalabra";
+        public static int contarLetra(String unaPalabra1, char letra){
+
+            int cuenta = 0;
+
+            String letraPalabra = "";
+            for (int i = 0; i<letraPalabra.length; i++){
+
+                if( letraPalabra.charAt(i)  == letra){
+
+                    cuenta = cuenta + 1;
+
+                }
+
+            }
+
+            return cuenta;
+
+        }
+    }
 }
